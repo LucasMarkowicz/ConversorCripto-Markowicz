55c0db9b918b69c35fc534a612a0a2d4301d43d7
LucasMarkowicz <luckpelle1@gmail.com>
2022-10-25 23:29:00 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,92 @@
+//eventos de botón
+let boton = document.getElementById("boton")
+boton.addEventListener("click", evitarDobleClick)
+boton.addEventListener("click", getValueInput)
+
+
+// función que evita doble click
+function evitarDobleClick() {
+    document.getElementById("boton").disabled = true;
+    setTimeout(function() {
+        document.getElementById("boton").disabled = false;
+    }, 50);
+}
+
+
+function getValueInput() {
+    document.getElementById("resultado").innerHTML = ""
+    document.getElementById("enunciado").innerHTML = ""
+    document.getElementById("hora").innerHTML = ""
+
+    let x = document.getElementById("criptoElegida");
+    let moneda = x.options[x.selectedIndex].text;
+
+    y = document.getElementById("ingreseValor");
+    let valor = y.value
+
+    // obtener la fecha y la hora
+    let today = new Date();
+    let now = today.toLocaleString();
+
+
+    // api de binance
+    let endpoint = 'https://api.binance.com/api/v3/ticker/price'
+    fetch(endpoint)
+        .then(respuesta => respuesta.json())
+        .then(data => mostrarData(data))
+        .catch(e => console.log(e))
+    
+
+    let mostrarData = (data) => {
+
+        class Criptomonedas {
+            constructor(nombreCorto, nombre, valorCripto, imagenCripto) {
+                this.short = nombreCorto
+                this.name = nombre;
+                this.valueC = valorCripto;
+                this.img = imagenCripto
+            }
+        }
+
+        const ETH = new Criptomonedas("ETH", "Ethereum", data[12].price, `<img src="media/ETH.png" alt="" >`)
+        const BTC = new Criptomonedas("BTC", "Bitcoin", data[11].price, `<img src="media/BTC.png" alt="" >`)
+        const BNB = new Criptomonedas("BNB", "Binance Coin", data[98].price, `<img src="media/BNB.png" alt="" >`)
+        const ADA = new Criptomonedas("ADA", "Cardano", data[296].price, `<img src="media/ADA.png" alt="" >`)
+        const SOL = new Criptomonedas("SOL", "Solana", data[779].price, `<img src="media/SOL.png" alt="" >`)
+        const USDC = new Criptomonedas("USDC", "Circle", 1, `<img src="media/USDC.png" alt="" >`)
+
+
+        listaCripto = [ETH, BTC, BNB, ADA, SOL, USDC];
+
+        //funcion conversion y muestra
+        function calculo(monto) {
+            let a = (parseFloat(valor) * monto / listaCripto[j].valueC);
+            document.getElementById("resultado").innerHTML += `Tu valor en ${listaCripto[j].img} ${listaCripto[j].name} (${listaCripto[j].short}) es ${a.toFixed(3)} <br><br>`
+        }
+    
+
+        let monedaElegida = listaCripto.find(element => moneda.includes(element.name));
+
+        if (valor == "") {
+            document.getElementById("resultado").innerHTML = "Por favor ingrese un valor númerico"
+            document.getElementById("enunciado").innerHTML = " "
+            document.getElementById("hora").innerHTML = " "
+        }
+        else {
+            for (j = 0; j < listaCripto.length; j++) {
+                if (j == listaCripto.indexOf(monedaElegida)) {
+
+                }
+                else {
+                    document.getElementById("enunciado").innerHTML = `<b>Elegiste convertir ${valor} ${monedaElegida.name} (${monedaElegida.short}) ${monedaElegida.img}</b> <br><br>`;
+                    document.getElementById("hora").innerHTML = `<b>Fecha y hora local:</b> ${now}`
+                    calculo(monedaElegida.valueC);
+                }
+            }
+        }
+    }
+}
+
+
+
+
